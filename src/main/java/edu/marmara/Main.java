d340fe4380767947d941ec5db8695bb16d127708
Umut Önder <umut.onder@obss.tech>
2022-11-10 19:10:02 +0300

    Initial Commit

---
+++ src/main/java/edu/marmara/Main.java
@@ -0,0 +1,7 @@
+package edu.marmara;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Initial Commit");
+    }
+}
---
+++ src/test/java/edu/marmara/MainTest.java
@@ -0,0 +1,9 @@
+package edu.marmara;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void contextLoad() {}
+}
