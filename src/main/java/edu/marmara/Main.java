695c31f425aee4998f5c4d7520f0fc187822af06
Firat BAKICI <nastary143@gmail.com>
2022-11-13 18:00:14 +0300

    Revert "First Implementation of View Package"
    This reverts commit 4339bd760d146ebe96bc5daa95eba034cf0bae4b.

--- src/main/java/edu/marmara/Main.java
+++ src/main/java/edu/marmara/Main.java
@@ -23,7 +23,6 @@ import edu.marmara.service.impl.CourseServiceImpl;
 import edu.marmara.service.impl.JsonServiceImpl;
 import edu.marmara.service.impl.SchoolServiceImpl;
 import edu.marmara.service.impl.StudentServiceImpl;
-import edu.marmara.view.View;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -33,7 +32,67 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static School school = School.getInstance();
+    public static JsonService jsonService = new JsonServiceImpl();
+    public static CourseService courseService = new CourseServiceImpl();
+    public static StudentService studentService = new StudentServiceImpl();
+    public static CourseMapper courseMapper = new CourseMapperImpl();
+    public static StudentRepository studentRepository = new StudentRepositoryImpl();
+    public static InstructorRepository instructorRepository = new InstructorRepositoryImpl();
+    public static StudentMapper studentMapper = new StudentMapperImpl();
+    public static Scanner scanner = new Scanner(System.in);
+    public static InstructorMapper instructorMapper = new InstructorMapperImpl();
+    public static SchoolService schoolService = new SchoolServiceImpl();
+
     public static void main(String[] args) throws IOException, ParseException {
-        View.start();
+        schoolService.uploadJsons();
+
+        while (true) {
+            System.out.println("Select User Type: \n1- Student\n2- Instructor\n9- Exit");
+
+            Integer input = scanner.nextInt();
+
+            if (input == 1) {
+                System.out.print("Enter your Student ID: ");
+                Student student = studentRepository.findByStudentId(scanner.nextLong());
+
+                if (student != null) {
+                    System.out.println(studentMapper.mapTo(student));
+
+                    for (Course course : studentService.getAvailableCourses(student)) {
+                        System.out.print(course.getCourseCode() + " " + course.getCourseTitle() + " ");
+                        for (WeeklyDate date : course.getDates()) {
+                            System.out.print(date.getDayName() + " " + date.getHours() + " ");
+                        }
+                        System.out.println();
+                    }
+
+                    System.out.print("\nEnter the course code to add it to your schedule: ");
+                    String courseCode = scanner.next();
+                    studentService.addCourseToSchedule(student, courseCode);
+                    System.out.println("\nYour schedule\n");
+
+                    for (Course course : student.getWeeklySchedule().getCourses()) {
+                        System.out.println(course.getCourseCode() + " " + course.getCourseTitle());
+                    }
+                } else {
+                    System.out.println("Cannot find the student with given ID");
+                }
+            } else if (input == 2) {
+                System.out.print("Enter your Email Address: ");
+                Instructor instructor = instructorRepository.findByEmail(scanner.next());
+
+                if (instructor != null) {
+                    System.out.println(instructorMapper.mapTo(instructor));
+                } else {
+                    System.out.println("Cannot find the instructor with given email");
+                }
+
+            } else if (input == 9) {
+                break;
+            } else {
+                System.out.println("Wrong input!");
+            }
+        }
     }
 }
